b03a389d2d267df1152e00db6b33d07f00cad747
mmullerx4 <mmullerx4@gmail.com>
2024-10-28 11:35:21 -0400

    w07-tutorial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'obs-01-start';
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
 }
