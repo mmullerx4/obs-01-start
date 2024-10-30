7b63bb0bc6c160ad01947c68eacbb0c4968615de
mmullerx4 <mmullerx4@gmail.com>
2024-10-30 19:03:08 -0400

    w07-tutorial

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,9 @@
-import
-{ EventEmitter,
-Injectable,
-Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+
+import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'}) //do this instead of adding to appmodule providers?
 export class UserService {
-  activateEmitter = new EventEmitter<boolean>();
+  //activateEmitter = new EventEmitter<boolean>(); //this is old way. You can now use a subject
+  activateEmitter = new Subject<boolean>();
 }
